635e0528aae602f86525dfa3e3cbcf2f86c1d7fd
Anthony Nizan <anthony.nizan@gmail.com>
2018-03-15 16:34:41 +0100

    First Commit, Add static class Convertor

---
+++ KataRomanNumberTests/ConvertorTestsShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using NUnit.Framework;
+
+namespace KataRomanNumberTests
+{
+    public class ConvertorTestsShould
+    {
+        [Test]
+        public void TranslateArabicNumberIntoRomanNumber()
+        {
+            Check.That(Convertor.ToRomanNumber(1)).IsEqualTo("I");
+        }
+    }
+
+    public static class Convertor
+    {
+        public static object ToRomanNumber(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KataRomanNumberTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KataRomanNumberTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KataRomanNumberTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7398c403-6040-4175-8161-a77b0d146d58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
